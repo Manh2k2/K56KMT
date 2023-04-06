d632ca8b6f6ee5db39c50ebea846a8131ed5dd12
duycop <duycop@hotmail.com>
2023-04-06 09:13:06 +0700

    đua xe

---
+++ Racing/Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace Racing
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.cmdLoad = new System.Windows.Forms.Button();
+            this.cmdStart = new System.Windows.Forms.Button();
+            this.timerRacing = new System.Windows.Forms.Timer(this.components);
+            this.labelKQ1 = new System.Windows.Forms.Label();
+            this.picRoad = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timerBlink = new System.Windows.Forms.Timer(this.components);
+            this.labelKQ2 = new System.Windows.Forms.Label();
+            this.labelKQ3 = new System.Windows.Forms.Label();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.picRoad)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cmdLoad
+            // 
+            this.cmdLoad.Location = new System.Drawing.Point(12, 12);
+            this.cmdLoad.Name = "cmdLoad";
+            this.cmdLoad.Size = new System.Drawing.Size(154, 50);
+            this.cmdLoad.TabIndex = 1;
+            this.cmdLoad.Text = "Load Cars";
+            this.cmdLoad.UseVisualStyleBackColor = true;
+            this.cmdLoad.Click += new System.EventHandler(this.cmdLoad_Click);
+            // 
+            // cmdStart
+            // 
+            this.cmdStart.Location = new System.Drawing.Point(172, 12);
+            this.cmdStart.Name = "cmdStart";
+            this.cmdStart.Size = new System.Drawing.Size(154, 50);
+            this.cmdStart.TabIndex = 2;
+            this.cmdStart.Text = "START";
+            this.toolTip1.SetToolTip(this.cmdStart, "OK");
+            this.cmdStart.UseVisualStyleBackColor = true;
+            this.cmdStart.Click += new System.EventHandler(this.cmdStart_Click);
+            // 
+            // timerRacing
+            // 
+            this.timerRacing.Interval = 10;
+            this.timerRacing.Tick += new System.EventHandler(this.timerLoad_Tick);
+            // 
+            // labelKQ1
+            // 
+            this.labelKQ1.AutoSize = true;
+            this.labelKQ1.Location = new System.Drawing.Point(358, 29);
+            this.labelKQ1.Name = "labelKQ1";
+            this.labelKQ1.Size = new System.Drawing.Size(24, 16);
+            this.labelKQ1.TabIndex = 8;
+            this.labelKQ1.Text = "1st";
+            // 
+            // picRoad
+            // 
+            this.picRoad.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.picRoad.Image = global::Racing.Properties.Resources.p1;
+            this.picRoad.Location = new System.Drawing.Point(0, 68);
+            this.picRoad.Name = "picRoad";
+            this.picRoad.Size = new System.Drawing.Size(1298, 578);
+            this.picRoad.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picRoad.TabIndex = 7;
+            this.picRoad.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.Image = global::Racing.Properties.Resources.p2;
+            this.pictureBox1.Location = new System.Drawing.Point(1298, 68);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(114, 578);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 11;
+            this.pictureBox1.TabStop = false;
+            // 
+            // timerBlink
+            // 
+            this.timerBlink.Interval = 1000;
+            this.timerBlink.Tick += new System.EventHandler(this.timerBlink_Tick);
+            // 
+            // labelKQ2
+            // 
+            this.labelKQ2.AutoSize = true;
+            this.labelKQ2.Location = new System.Drawing.Point(682, 29);
+            this.labelKQ2.Name = "labelKQ2";
+            this.labelKQ2.Size = new System.Drawing.Size(29, 16);
+            this.labelKQ2.TabIndex = 12;
+            this.labelKQ2.Text = "2nd";
+            // 
+            // labelKQ3
+            // 
+            this.labelKQ3.AutoSize = true;
+            this.labelKQ3.Location = new System.Drawing.Point(1011, 29);
+            this.labelKQ3.Name = "labelKQ3";
+            this.labelKQ3.Size = new System.Drawing.Size(26, 16);
+            this.labelKQ3.TabIndex = 13;
+            this.labelKQ3.Text = "3rd";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1410, 645);
+            this.Controls.Add(this.labelKQ3);
+            this.Controls.Add(this.labelKQ2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.labelKQ1);
+            this.Controls.Add(this.cmdStart);
+            this.Controls.Add(this.cmdLoad);
+            this.Controls.Add(this.picRoad);
+            this.Name = "Form1";
+            this.Text = "RACING CARS - 56KMT";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            ((System.ComponentModel.ISupportInitialize)(this.picRoad)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button cmdLoad;
+        private System.Windows.Forms.Button cmdStart;
+        private System.Windows.Forms.PictureBox picRoad;
+        private System.Windows.Forms.Timer timerRacing;
+        private System.Windows.Forms.Label labelKQ1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer timerBlink;
+        private System.Windows.Forms.Label labelKQ2;
+        private System.Windows.Forms.Label labelKQ3;
+        private System.Windows.Forms.ToolTip toolTip1;
+    }
+}
+
---
+++ Racing/Form1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Racing
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string[] sv = { "Đỗ Tuấn Anh", "Vũ Công Anh", "Nguyễn Tá Anh", "Vũ Ngọc Bình", "Nguyễn Duy Cao", "Hoàng Đức Chung", "Nguyễn Thế Định", "Phommaly Dobee", "Bùi Minh Đức", "Phạm Tuấn Dương", "Phương Minh Duy", "Ma Bách Duy", "Nguyễn Thị Mỹ Duyên", "Đặng Thị Hà", "Đỗ Văn Hiếu", "Hà Trung Hiếu", "Nguyễn Thị Minh Hoa", "Trần Thái Học", "Trần Quang Huy", "Hà Văn Khánh", "Triệu Văn Lương", "Nguyễn Đức Mạnh", "Kouson Mosaky", "Giáp Văn Nam", "Dương Thị Bích Nguyệt", "Dương Văn Ninh", "Hoàng Tràn Phâu", "Vi Duy Quốc", "Pon Sokphal", "Mao Sorakpheanukma", "Nguyễn Ngọc Thái", "Nguyễn Thị Thảo", "Lê Việt Tiến", "Hoàng Mạnh Tiến", "Lê Đức Toàn", "Nguyễn Phương Trâm", "Vũ Ngọc Trang", "Trần Nhật Trường", "Vi Anh Tuấn", "Kun Vengann", "Lưu Viết Vĩ", "Vilaphon Viengsavanh", "Xèn Đức Việt", "Nguyễn Phi Vũ" };
+        private void cmdStart_Click(object sender, EventArgs e)
+        {
+            timerRacing.Enabled = !timerRacing.Enabled;
+        }
+
+        Random random = new Random(DateTime.Now.Millisecond);
+        Bitmap rmCarColor()
+        {
+            int r = random.Next(1, 4);
+            switch (r)
+            {
+                case 1:
+                    return Properties.Resources.red_car;
+                case 2:
+                    return Properties.Resources.yellow_car;
+                case 3:
+                    return Properties.Resources.green_car;
+            }
+            return Properties.Resources.red_car;
+        }
+        PictureBox[] allCars;
+        private void cmdLoad_Click(object sender, EventArgs e)
+        {
+            labelKQ1.Text = "1st";
+            labelKQ2.Text = "2nd";
+            labelKQ3.Text = "3rd";
+            timerRacing.Stop();
+            timerBlink.Stop();
+            if (allCars != null && allCars.Length > 0)
+            {
+                foreach (PictureBox car in allCars)
+                {
+                    car.Dispose();
+                }
+            }
+            max = 0;
+            int w = 65, h = 13, dw = 35, dh = 50, n = 44;
+            allCars = new PictureBox[n];
+            for (int i = 0; i < n; i++)
+            {
+                PictureBox p = new PictureBox();
+                p.BackColor = Color.Transparent;
+                p.Image = rmCarColor();
+                p.Size = new Size(w, h);
+                p.Location = new Point(dw, dh + (h + 1) * i);
+                p.SizeMode = PictureBoxSizeMode.StretchImage;
+                p.Tag = i;
+                this.toolTip1.SetToolTip(p, $"sv[{i+1}] = {sv[i]}");
+                picRoad.Controls.Add(p);
+                allCars[i] = p;
+            }
+        }
+
+        int max,i1, i2, i3;
+        private void timerLoad_Tick(object sender, EventArgs e)
+        {
+            if (allCars != null && allCars.Length > 0)
+            {
+                foreach (PictureBox car in allCars)
+                {
+                    int dx = random.Next(0, 22);
+                    car.Left += dx;
+
+                    if (car.Left > max)
+                    {
+                        max = car.Left;
+                        if (max + car.Width > picRoad.Width)
+                        {
+                            timerRacing.Stop();
+                            win = car;
+                            timerBlink.Start();
+                            break;
+                        }
+                    }
+                }
+                PictureBox p1 = GetMaxBelow(this.Width,null);
+                i1 = (int)p1.Tag;
+                labelKQ1.Text = $"1st: sv[{i1 + 1}] = {sv[i1]}";
+                PictureBox p2 = GetMaxBelow(p1.Left,p1);
+                i2 = (int)p2.Tag;
+                labelKQ2.Text = $"2nd: sv[{i2 + 1}] = {sv[i2]}";
+                PictureBox p3 = GetMaxBelow(p2.Left,p2);
+                i3 = (int)p3.Tag;
+                labelKQ3.Text = $"3rd: sv[{i3 + 1}] = {sv[i3]}";
+            }
+        }
+        PictureBox GetMaxBelow(int MAX, PictureBox box)
+        {
+            int max = 0;
+            PictureBox p = null;
+            foreach (PictureBox car in allCars) if (car != box)
+                {
+                    int x = car.Left;
+                    if (x > max && x <= MAX)
+                    {
+                        max = x;
+
+                        p = car;
+                    }
+                }
+            return p;
+        }
+        PictureBox win;
+        private void timerBlink_Tick(object sender, EventArgs e)
+        {
+            win.Visible = !win.Visible;
+            timerBlink.Interval = win.Visible ? 1000 : 40;
+        }
+    }
+}
