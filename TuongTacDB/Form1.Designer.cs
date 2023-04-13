edc849b8a2b1899dcf82a75b3a08143eb6c88706
duycop <duycop@hotmail.com>
2023-04-13 11:00:42 +0700

    tương tác db

---
+++ TuongTacDB/Form1.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace TuongTacDB
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnViewAll = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.cmdLoadID_DD = new System.Windows.Forms.Button();
+            this.btnUpdateMuon = new System.Windows.Forms.Button();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.mnuMuon_Vang = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuVang_diHoc = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuVang_Muon = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuDihoc_Vang = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnViewAll
+            // 
+            this.btnViewAll.Location = new System.Drawing.Point(12, 44);
+            this.btnViewAll.Name = "btnViewAll";
+            this.btnViewAll.Size = new System.Drawing.Size(176, 45);
+            this.btnViewAll.TabIndex = 0;
+            this.btnViewAll.Text = "View ALL SV";
+            this.btnViewAll.UseVisualStyleBackColor = true;
+            this.btnViewAll.Click += new System.EventHandler(this.btnViewAll_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.ContextMenuStrip = this.contextMenuStrip1;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 109);
+            this.dataGridView1.MultiSelect = false;
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(776, 460);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(219, 68);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(226, 21);
+            this.comboBox1.TabIndex = 2;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // cmdLoadID_DD
+            // 
+            this.cmdLoadID_DD.Location = new System.Drawing.Point(219, 16);
+            this.cmdLoadID_DD.Name = "cmdLoadID_DD";
+            this.cmdLoadID_DD.Size = new System.Drawing.Size(136, 46);
+            this.cmdLoadID_DD.TabIndex = 3;
+            this.cmdLoadID_DD.Text = "Lấy data diemDanh";
+            this.cmdLoadID_DD.UseVisualStyleBackColor = true;
+            this.cmdLoadID_DD.Click += new System.EventHandler(this.cmdLoadID_DD_Click);
+            // 
+            // btnUpdateMuon
+            // 
+            this.btnUpdateMuon.Location = new System.Drawing.Point(489, 43);
+            this.btnUpdateMuon.Name = "btnUpdateMuon";
+            this.btnUpdateMuon.Size = new System.Drawing.Size(136, 46);
+            this.btnUpdateMuon.TabIndex = 5;
+            this.btnUpdateMuon.Text = "Vắng -> Đi muộn";
+            this.btnUpdateMuon.UseVisualStyleBackColor = true;
+            this.btnUpdateMuon.Click += new System.EventHandler(this.btnUpdateMuon_Click);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuMuon_Vang,
+            this.mnuVang_diHoc,
+            this.mnuVang_Muon,
+            this.mnuDihoc_Vang});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(181, 114);
+            // 
+            // mnuMuon_Vang
+            // 
+            this.mnuMuon_Vang.Name = "mnuMuon_Vang";
+            this.mnuMuon_Vang.Size = new System.Drawing.Size(180, 22);
+            this.mnuMuon_Vang.Text = "Muộn -> Vắng";
+            this.mnuMuon_Vang.Click += new System.EventHandler(this.mnuMuon_Vang_Click);
+            // 
+            // mnuVang_diHoc
+            // 
+            this.mnuVang_diHoc.Name = "mnuVang_diHoc";
+            this.mnuVang_diHoc.Size = new System.Drawing.Size(180, 22);
+            this.mnuVang_diHoc.Text = "Vắng -> Đi học";
+            this.mnuVang_diHoc.Click += new System.EventHandler(this.mnuVang_diHoc_Click);
+            // 
+            // mnuVang_Muon
+            // 
+            this.mnuVang_Muon.Name = "mnuVang_Muon";
+            this.mnuVang_Muon.Size = new System.Drawing.Size(180, 22);
+            this.mnuVang_Muon.Text = "Vắng -> Muộn";
+            this.mnuVang_Muon.Click += new System.EventHandler(this.mnuVang_Muon_Click);
+            // 
+            // mnuDihoc_Vang
+            // 
+            this.mnuDihoc_Vang.Name = "mnuDihoc_Vang";
+            this.mnuDihoc_Vang.Size = new System.Drawing.Size(180, 22);
+            this.mnuDihoc_Vang.Text = "Đi học -> Vắng";
+            this.mnuDihoc_Vang.Click += new System.EventHandler(this.mnuDihoc_Vang_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 581);
+            this.Controls.Add(this.btnUpdateMuon);
+            this.Controls.Add(this.cmdLoadID_DD);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnViewAll);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnViewAll;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button cmdLoadID_DD;
+        private System.Windows.Forms.Button btnUpdateMuon;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem mnuMuon_Vang;
+        private System.Windows.Forms.ToolStripMenuItem mnuVang_diHoc;
+        private System.Windows.Forms.ToolStripMenuItem mnuVang_Muon;
+        private System.Windows.Forms.ToolStripMenuItem mnuDihoc_Vang;
+    }
+}
+
---
+++ TuongTacDB/Form1.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace TuongTacDB
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        //view all ds sv
+        private void btnViewAll_Click(object sender, EventArgs e)
+        {
+            //gọi hàm để sql->data
+            DataTable dt = getData("select *  from sv order by stt");
+            dataGridView1.DataSource = dt; //data -> display
+        }
+
+
+        //hàm dùng nhiều lần
+        DataTable getData(string sql, int id_dd = 0, CommandType loaiSQL = CommandType.Text, string masv = null, int target=-1)
+        {
+            /* quy trình lấy data from sql server
+            * step1: kết nối tới db, dùng đối tượng kết nối: SqlConnection
+            *   tạo đt
+            *   gắn chuỗi kết nối: tra cứu https://www.connectionstrings.com/
+            *   open() : đây là kết nối
+            * step2: sử dụng đối tượng thực thi lệnh SQL : SqlCommand
+            *   tạo đối tượng, gắn với đối tượng kết nối
+            *   gắn chuỗi sql: chuỗi này sẽ quyết định lấy data gì
+            *   truyền tham số (nếu có)
+            *   gọi phương thức để lấy dữ liệu -> biến
+            * step3: view kết quả
+            *   xứ lý biến nhận được -> chuyển về dạng dataTable
+            *   đưa lên đối tượng hiển thị: DataGridView
+            */
+
+            //step1:
+            SqlConnection sqlConnection = new SqlConnection();
+            sqlConnection.ConnectionString = "Server=LAPTOP-DUYCOP\\SQLEXPRESS;Database=svKMT_tnut;Trusted_Connection=True;";
+            sqlConnection.Open();
+
+            //step2
+            SqlCommand sqlCommand = sqlConnection.CreateCommand();
+            sqlCommand.CommandType = loaiSQL;
+            sqlCommand.CommandText = sql;
+            if (id_dd != 0)
+            {
+                //truyền tham số
+                sqlCommand.Parameters.Add("@id", SqlDbType.Int).Value = id_dd;
+            }
+            if (target != -1)
+            {
+                //truyền tham số
+                sqlCommand.Parameters.Add("@target", SqlDbType.Int).Value = target;
+            }
+            if (masv != null)
+            {
+                //truyền tham số
+                sqlCommand.Parameters.Add("@masv", SqlDbType.NVarChar, 50).Value = masv;
+            }
+            SqlDataReader sdr = sqlCommand.ExecuteReader();
+            //step3
+            DataTable dt = new DataTable();
+            dt.Load(sdr); //lấy toàn bộ dữ liệu trong sdr vào dt
+
+            //giải phóng bộ nhớ
+            sqlCommand.Dispose();
+            sqlConnection.Close();
+            sqlConnection.Dispose();
+
+            //trả về dữ liệu
+            return dt;
+        }
+        List<int> MangID = new List<int>();
+        private void cmdLoadID_DD_Click(object sender, EventArgs e)
+        {
+            DataTable dt_dd = getData("select * from DiemDanh");
+            comboBox1.Items.Clear(); //xoá sạch
+            MangID.Clear();
+            foreach (DataRow dr in dt_dd.Rows)
+            {
+                comboBox1.Items.Add($"id={dr["id"]} ngày {dr["ngay"]}");
+                MangID.Add((int)dr["id"]);
+            }
+            comboBox1.SelectedIndex = comboBox1.Items.Count - 1;
+        }
+
+        private void btnChiTiet_Click(object sender, EventArgs e)
+        {
+            int firstView = dataGridView1.FirstDisplayedScrollingRowIndex;
+            int index_selected = -1;
+            bool dang_selected = false;
+            if (dataGridView1.SelectedRows.Count > 0)
+            {
+                dang_selected = true;
+                DataGridViewRow r = dataGridView1.SelectedRows[0];
+                index_selected = r.Index;
+            }
+
+            int index = comboBox1.SelectedIndex;
+            int id = MangID[index];
+            DataTable dt = getData("SP_DiemDanh", id, CommandType.StoredProcedure);
+            dataGridView1.DataSource = dt;
+
+            if (dang_selected && index_selected > -1)
+            {
+                dataGridView1.FirstDisplayedScrollingRowIndex = firstView;
+                dataGridView1.Rows[index_selected].Selected = true;
+            }
+        }
+
+        private void btnUpdateMuon_Click(object sender, EventArgs e)
+        {
+            BienDoi("Vắng mặt", 2);
+        }
+
+        void BienDoi(string From, int target) { 
+            //chọn 1 dòng trên lưới
+            //nếu dòng đó nói sv Vắng mặt
+            //thì: lấy masv, id buổi điểm danh
+            //trường: Vang ==1 <=> Vắng mặt
+            //change: Vang -> 2 <=> Muộn
+
+            if (dataGridView1.SelectedRows.Count > 0)
+            {
+                int firstView = dataGridView1.FirstDisplayedScrollingRowIndex;
+
+                //bỏ multi: nên chỉ có 1 row đc chọn
+                DataGridViewRow r = dataGridView1.SelectedRows[0];
+                int index_selected = r.Index; //lưu index của thằng -> Muộn
+                string masv = r.Cells["masv"].Value.ToString();
+
+                int index = comboBox1.SelectedIndex;
+                int id = MangID[index];
+
+                string kq = r.Cells["Điểm Danh"].Value.ToString();
+
+                if (kq == From)
+                {
+                    //tương tác db để biến sv này Vắng -> Muộn
+                    string sql = "sp_biendoi";
+                    getData(sql, id, CommandType.StoredProcedure, masv, target);
+
+                    //tự động tải lại ds, 
+                    btnChiTiet_Click(null, null);
+                    //dòng update->Muộn vẫn là dòng đang chọn
+                    dataGridView1.Rows[index_selected].Selected = true;
+                    dataGridView1.FirstDisplayedScrollingRowIndex = firstView;
+                   //MessageBox.Show("OK");
+                }
+                else
+                    MessageBox.Show($" sv massv = {masv} : {kq}", "Không xử lý");
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox1.Items.Count > 0)
+            {
+                btnChiTiet_Click(sender, e);
+            }
+        }
+
+        private void mnuVang_Muon_Click(object sender, EventArgs e)
+        {
+            BienDoi("Vắng mặt", 2);
+        }
+
+        private void mnuMuon_Vang_Click(object sender, EventArgs e)
+        {
+            BienDoi("Đi muộn", 1);
+        }
+
+        private void mnuVang_diHoc_Click(object sender, EventArgs e)
+        {
+            BienDoi("Vắng mặt", 0);
+        }
+
+        private void mnuDihoc_Vang_Click(object sender, EventArgs e)
+        {
+            BienDoi("Đi học", 1);
+        }
+    }
+}
